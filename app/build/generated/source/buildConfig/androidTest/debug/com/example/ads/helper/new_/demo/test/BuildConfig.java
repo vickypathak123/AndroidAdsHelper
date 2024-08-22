55b0aa10df10e3e8b7ad2511a1d770c26c6a6940
Akshay Harsoda <akshay2.vasundharacoaching@gmail.com>
2024-08-22 12:02:31 +0530

    v1.0.0 - Initial commit

--- app/build/generated/source/buildConfig/androidTest/debug/com/example/ads/helper/new_/demo/test/BuildConfig.java
+++ app/build/generated/source/buildConfig/androidTest/debug/com/example/ads/helper/new_/demo/test/BuildConfig.java
@@ -5,6 +5,6 @@ package com.example.ads.helper.new_.demo.test;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "com.voice.gps.navigation.map.location.route.test";
+  public static final String APPLICATION_ID = "com.example.ads.helper.new_.demo.test";
   public static final String BUILD_TYPE = "debug";
 }
--- app/build/generated/source/buildConfig/debug/com/example/ads/helper/new_/demo/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/example/ads/helper/new_/demo/BuildConfig.java
@@ -5,8 +5,8 @@ package com.example.ads.helper.new_.demo;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "com.voice.gps.navigation.map.location.route";
+  public static final String APPLICATION_ID = "com.example.ads.helper.new_.demo";
   public static final String BUILD_TYPE = "debug";
   public static final int VERSION_CODE = 1;
-  public static final String VERSION_NAME = "2.0.3";
+  public static final String VERSION_NAME = "1.0.0";
 }
