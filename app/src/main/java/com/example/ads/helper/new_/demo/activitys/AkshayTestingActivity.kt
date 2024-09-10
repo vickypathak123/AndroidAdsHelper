4220988a3efda94c11469c8ed5c2b524cdd2e743
Akshay Harsoda <akshay2.vasundharacoaching@gmail.com>
2024-09-10 13:57:51 +0530

    Update SAFETYNET_README.md

--- app/src/main/java/com/example/ads/helper/new_/demo/activitys/AkshayTestingActivity.kt
+++ app/src/main/java/com/example/ads/helper/new_/demo/activitys/AkshayTestingActivity.kt
@@ -4,8 +4,45 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class AkshayTestingActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+//        AppProtector.with(this@AkshayTestingActivity)
+//            .appName(appName = "YOUR APP NAME")
+//            .packageName(packageName = "YOUR APP PACKAGE NAME")
+//            .cloudProjectNumber(cloudProjectNumber = YOUR CLOUD PROJECT NUMBER)
+//            .playIntegrityRemoteConfigJson(remoteConfigJson = "PLAY INTEGRITY REMOTE CONFIG JSON")
+//
+//            /**
+//             * Helper method to set deviceId which you can get from logs
+//             * it's required field to set debug testing for google-consent
+//             * Check your logcat output for the hashed device ID e.g.
+//             * "Use new ConsentDebugSettings.Builder().addTestDeviceHashedId("ABCDEF012345")" to use
+//             */
+//
+//            .setTestDeviceId(deviceId = "YOUR TEST DEVICE ID")
+//
+//            /**
+//             * enable debug mode [by Default value = false]
+//             * for check in test device and check all type of logs in Logcat
+//             */
+//            .enableDebugMode(fIsEnable = false)
+//
+//            /**
+//             * for testing purpose
+//             * need to block check integrity [by Default value = false]
+//             * it pass true then not check play Integrity.
+//             */
+//            .needToBlockCheckIntegrity(fIsBlock = false)
+//
+//            /**
+//             * check integrity
+//             * callback when devise passed all Safetynet params
+//             */
+//            .checkIntegrity {
+//                // TODO: perform your next action
+//            }
 
 //        /**
 //         * initialization of Notification Data
